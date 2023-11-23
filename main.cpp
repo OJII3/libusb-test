3cacab904d8e1787bc75734de599c019368c8bc8
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-11-23 14:21:03 +0900

    [add]: opening operation

--- main.cpp
+++ main.cpp
@@ -1,25 +1,60 @@
+#include <array>
 #include <iostream>
 #include <libusb-1.0/libusb.h>
 
 auto main() -> int {
+
+    // create a context and initialize
     libusb_context *ctx = nullptr;
     libusb_init(&ctx);
-    libusb_device **devs = nullptr;
-    auto cnt = libusb_get_device_list(ctx, &devs);
-    std::cout << "Found: " << cnt << " devices" << std::endl;
 
-    for (auto i = 0; i < sizeof(devs); ++i) {
+    libusb_device **devs = nullptr;
+    auto device_count = libusb_get_device_list(ctx, &devs);
+
+    if (device_count < 0) {
+        std::cerr << "Failed to get device list" << std::endl;
+        return -1;
+    }
+    std::cout << "Found: " << device_count << " devices." << std::endl;
+
+    libusb_device_handle *handle = nullptr;
+
+    for (auto i = 0; i < device_count; ++i) {
         auto dev = devs[i];
         libusb_device_descriptor desc{};
         libusb_get_device_descriptor(dev, &desc);
-        libusb_config_descriptor *config = nullptr;
-        libusb_get_config_descriptor(dev, 0, &config);
-        std::cout << "Device: " << desc.idVendor << ":" << desc.bcdDevice
-                  << ", " << desc.idProduct << ", " << desc.iSerialNumber
-                  << ", " << desc.bDeviceClass << ", " << desc.iManufacturer
-                  << ", " << desc.bcdUSB << ", " << desc.bMaxPacketSize0
+
+        std::cout << "\nDevice: " << desc.idVendor
+                  << "\n idProduct: " << desc.idProduct
+                  << "\n Bus: " << int(libusb_get_bus_number(dev))
+                  << "\n Port: " << int(libusb_get_port_number(dev))
+                  << "\n Speed: " << int(libusb_get_device_speed(dev))
                   << std::endl;
-        std::cout << "Config: " << config->bLength << std::endl;
+
+        if (desc.iProduct == 0) {
+            std::cerr << "No product name" << std::endl;
+        } else {
+            auto result = libusb_open(dev, &handle);
+            if (result != 0) {
+                std::cerr << "Failed to open device. Skipping" << std::endl;
+                std::cerr << "libusb_open error: " << libusb_strerror(result)
+                          << std::endl;
+            } else {
+                std::cout << "Opened device" << std::endl;
+
+                char product_name[256];
+                libusb_get_string_descriptor_ascii(
+                    handle, desc.iProduct,
+                    static_cast<unsigned char *>(
+                        static_cast<void *>(product_name)),
+                    sizeof(product_name));
+
+                std::cout << "Product: " << std::string(product_name)
+                          << std::endl;
+
+                libusb_close(handle);
+            }
+        }
     }
 
     return 0;
